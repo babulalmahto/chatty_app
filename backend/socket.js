1cefec5d448577956543992a370e5b20656c29f0
Babulal Mahto <dev.babulalmahto@gmail.com>
2026-08-14 00:22:02 +0530

    Add socket helper functions for managing chat rooms and user disconnections

--- backend/socket.js
+++ backend/socket.js
@@ -1,3 +1,5 @@
+import { leaveAllRooms } from "./socket/helpers.js";
+
 export const initalizeSocket = async (io) => {
   io.on("connection", async (socket) => {
     // console.log("User connected=====>", socket.id);
@@ -5,6 +7,11 @@ export const initalizeSocket = async (io) => {
       const user = socket.user;
       console.log("User connected", user.id);
       socket.join(user._id.toString());
+
+      socket.on("disconnect", async () => {
+        leaveAllRooms(socket);
+        console.log("User disconnected", user.id);
+      });
     } catch (error) {
       console.error("Socket connection error:", error);
       socket.emit("internal_error", { error: "Internal server error" });
